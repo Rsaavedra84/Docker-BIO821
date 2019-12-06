1431913b134037b9d7be1213a4e6e821782c9561
joaquin <jm622@duke.edu>
2019-12-06 17:44:59 -0500

    added the querry to generate the dataframe, and aslo the plot

--- analysis.py
+++ analysis.py
@@ -1,17 +1,22 @@
-import pandas
+import pandas as pd
 import sqlalchemy
 import matplotlib.pyplot as plt
+import seaborn as sbn
 
 
 ''' Derive a sql query that returns a data set that contains a column for country, a
 column for season, and a column for average number of goals scored per game for each country-season combination. '''
 
-engine = sqlalchemy.create_engine('sqlite:///data/database.sqlite'))
+engine = sqlalchemy.create_engine('sqlite:///data/database.sqlite')
 conn = engine.connect()
 df = conn.execute('''
-    SELECT Country.id , name,  home_team_goal, away_team_goal , season , AVG(home_team_goal + away_team_goal) AS Avg_all_goals
-    FORM Country
+    SELECT name, season , AVG(home_team_goal + away_team_goal) AS Avg_all_goals
+    FROM Country
     LEFT JOIN Match
     ON Country.id = Match.country_id
     GROUP BY country.id , season
     ; ''').fetchall()
+
+df = pd.DataFrame(data= df, columns = ['Country','Season', 'Avg_goals_per_game'])
+
+sbn.lineplot(data = df, x='Season', y='Avg_goals_per_game', hue='Country')
