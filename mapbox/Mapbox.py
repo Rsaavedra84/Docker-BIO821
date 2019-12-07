bd83cfefe69187cc52666b27ccda90d2dcba7c8a
joaquin <jm622@duke.edu>
2019-12-07 17:54:08 -0500

    Mapbox class. Currently working declaring the private token. Need to try on linux env

---
+++ mapbox/Mapbox.py
@@ -0,0 +1,41 @@
+import sqlalchemy
+
+class Mapbox:
+    '''
+    Some definition of this class
+    '''
+
+    def __init__(self, country_name, private_token ):
+        '''
+
+        :param country_name:
+        '''
+        self.country_name = country_name
+        self.id = None
+        #this would be allowed in the final version.
+        self.private_token = private_token
+        #self.private_token = os.environ['MAPBOX_PRIVATE_TOKEN']
+        self.url = "https://api.mapbox.com/geocoding/v5/mapbox.places/{}.json?types=country,region&access_token=".format(self.country_name) + self.private_token
+        self.lat = None
+        self.long = None
+        self.country_id = None
+
+
+    def database (self, db):
+        '''
+
+        :param db:
+        :return:
+        '''
+        self.db = 'sqlite:///' + db
+        self.engine = sqlalchemy.create_engine(self.db)
+        self.conn = self.engine.connect()
+
+    def query (self):
+        '''
+
+        :return:
+        '''
+        self.id = self.conn.execute(''' SELECT id 
+                                        FROM Country
+                         WHERE name = "{}";'''.format(self.country_name)).fetchone()[0]
