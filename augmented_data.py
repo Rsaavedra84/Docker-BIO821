9b0dce1b5f7908f0c6150e91807851cc19baa3ba
joaquin <jm622@duke.edu>
2019-12-07 16:14:48 -0500

    added private token

--- augmented_data.py
+++ augmented_data.py
@@ -4,16 +4,16 @@ import sqlalchemy
 import os
 
 df = pd.read_sql_table('Country','sqlite:///data/database.sqlite')
-countries = df['name'].unique() #unique countries
+countries = df['name'].unique() # unique countries
 
 public_token = "pk.eyJ1IjoibWVsbGlqb2FjbyIsImEiOiJjazNwNTc3a2YyOGp1M2RsZHNmcXIwdG5nIn0.hCauisNqiuFuVAnwiH7AQA"
-# private_token = os.environ['MAPBOX_PRIVATE_TOKEN'] # check this in the future
+private_token = os.environ['MAPBOX_PRIVATE_TOKEN'] # check this in the future
 
 all_countries = {}
 # there is a problem. MAPBOX do not consider countries inside UK as countries. It assigns the 'region' id.
 
 for country in countries:
-    query = "https://api.mapbox.com/geocoding/v5/mapbox.places/{}.json?types=country,region&access_token=".format(country) + public_token
+    query = "https://api.mapbox.com/geocoding/v5/mapbox.places/{}.json?types=country,region&access_token=".format(country) + private_token
     data = requests.get(query).json()
     all_countries[country] = data['features'] # data from mapbox for every country in our table
 
